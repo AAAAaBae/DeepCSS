5dc076b714943bc7c4e1b076994fb0122d7616e6
AaAaAa <48274111+AAAAaBae@users.noreply.github.com>
2025-01-11 13:00:18 +0800

    Update ML_Model.py

--- ML_Model.py
+++ ML_Model.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import GridSearchCV, KFold
 from xgboost.sklearn import XGBClassifier
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
 from imblearn.over_sampling import SMOTE
-from scipy.stats import spearmanr
 from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_score
 from sklearn.metrics import classification_report, cohen_kappa_score, matthews_corrcoef
 from sklearn.preprocessing import MinMaxScaler
@@ -51,42 +50,35 @@ k_best = SelectKBest(score_func=chi2, k=35)
 x_chi = k_best.fit_transform(x, y)
 x_chi = pd.DataFrame(x_chi)
 print(x_chi.head())
-x_train, x_test, y_train, y_test = train_test_split(x_chi, y, test_size=0.15, random_state=42)
+
+cv = KFold(n_splits=5, random_state=42, shuffle=True)
 
 # RF
 # ml = RandomForestClassifier(n_estimators=200, max_depth=20, min_samples_leaf=1, min_samples_split=2, random_state=42)
-# ml.fit(x_train, y_train)
 
 # DT
 # ml = DecisionTreeClassifier(max_depth=5, random_state=42)  # initialization max_depth=5, min_samples_split=20, min_samples_leaf=5, max_features=5, max_leaf_nodes=80
-# ml.fit(x_train, y_train)
 
 # MLP
-# ml = MLPClassifier(activation='relu', solver='adam', alpha=0.5)
-# ml.fit(x_train, y_train)
+# ml = MLPClassifier(activation='relu', solver='adam', alpha=0.1)
 
 # LR
 # ml = LogisticRegression(random_state=42)
-# ml.fit(x_train, y_train)
 
 # KNN
 # ml = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=45, p=2)
-# ml.fit(x_train, y_train)
 
 # XGBoost
 # ml = XGBClassifier(num_class=3, colsample_bytree=0.8, learning_rate=0.1, max_depth=10, n_estimators=200, objective='multi:softmax', subsample=0.6)
-# ml.fit(x_train, y_train)
 
 # AdaBoost
 # ml = AdaBoostClassifier(DecisionTreeClassifier(max_depth=5, min_samples_split=20, min_samples_leaf=5),
 #                         random_state=42,
 #                         algorithm="SAMME.R",
 #                         n_estimators=200, learning_rate=0.5)
-# ml.fit(x_train, y_train)
 
 # GraindentBoost
 ml = GradientBoostingClassifier(n_estimators=150, learning_rate=0.5, max_depth=3, random_state=42)
-ml.fit(x_train, y_train)
 # *********************************************** GridSearch-GradientBoost ******************************************************
 # param_grid = {
 #     'n_estimators': [50, 100, 150, 200],
@@ -143,17 +135,22 @@ ml.fit(x_train, y_train)
 # grid_search.fit(x_train, y_train)
 # print("Best parameters found：", grid_search.best_params_)
 # *****************************************************************************************************************
+y_pred_list = []
+y_test_list = []
 
-cv_scores = cross_val_score(ml, x_chi, y, cv=5, scoring='accuracy')
-print("5-fold：", cv_scores)
-print("avg：", np.mean(cv_scores))
-accuracy = cv_scores.mean()
-precision = cross_val_score(ml, x_chi, y, cv=5, scoring='precision_macro').mean()
-recall = cross_val_score(ml, x_chi, y, cv=5, scoring='recall_macro').mean()
-f1 = cross_val_score(ml, x_chi, y, cv=5, scoring='f1_macro').mean()
+for train_index, test_index in cv.split(x_chi, y):
+    x_train, x_test = x_chi.iloc[train_index], x_chi.iloc[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
-y_pred = ml.predict(x_test)
-print(classification_report(y_test, y_pred, digits=5))
-corr, p_value = spearmanr(y_test, y_pred)
-print("spearman：", corr)
-print("P_value：", p_value)
+    ml.fit(x_train, y_train)
+
+    y_pred = ml.predict(x_test)  # KNN np.array(x_test)
+    y_pred_list.append(y_pred)
+    y_test_list.append(y_test)
+
+for i in range(len(y_pred_list)):
+    y_test = y_test_list[i]
+    y_pred = y_pred_list[i]
+    print(f"The{i + 1} fold classification report：")
+    print(classification_report(y_test, y_pred, digits=5))
+    print(f"The{i + 1} fold MCC：", matthews_corrcoef(y_test, y_pred))
